a3608774ce6cf46843564610881e51370a9661b7
joe <r.zamorskiy@yandex.ru>
2023-03-13 11:45:26 +0300

    added assertion in test

--- tests/test_headers_method.py
+++ tests/test_headers_method.py
@@ -5,4 +5,3 @@ class TestCookieMethod:
     def test_method_cookie(self):
         cookie = requests.get('https://playground.learnqa.ru/api/homework_cookie').cookies
         print(f'cookie is "{cookie}"')
-        assert cookie != "", f'Cant find cookie in response'
