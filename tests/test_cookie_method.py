42fb7c9a0515b36a71ebee0264ecb9e272cecede
joe <r.zamorskiy@yandex.ru>
2023-03-13 11:51:02 +0300

    delete

--- tests/test_cookie_method.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-class TestCookieMethod:
-    def test_method_cookie(self):
-        cookie = requests.get('https://playground.learnqa.ru/api/homework_cookie').cookies
-        print(f'cookie is "{cookie}"')
-        assert cookie != "", f'Cant find cookie in response'
--- tests/test_headers_method.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-class TestCookieMethod:
-    def test_method_cookie(self):
-        cookie = requests.get('https://playground.learnqa.ru/api/homework_cookie').cookies
-        print(f'cookie is "{cookie}"')
