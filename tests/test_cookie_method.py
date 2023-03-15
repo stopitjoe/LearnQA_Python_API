142be868a44b8c04a56bfafb8a4a56c43f7ef9c4
Roman <r.zamorskiy@yandex.ru>
2023-03-15 12:16:42 +0300

    reforge test_cookie_method

--- tests/test_cookie_method.py
+++ tests/test_cookie_method.py
@@ -4,5 +4,6 @@ import requests
 class TestCookieMethod:
     def test_method_cookie(self):
         cookie = requests.get('https://playground.learnqa.ru/api/homework_cookie').cookies
+        expected_cookie = "<RequestsCookieJar[<Cookie HomeWork=hw_value for .playground.learnqa.ru/>]>"
         print(f'cookie is "{cookie}"')
-        assert cookie != "", f'Cant find cookie in response'
+        assert str(cookie) == expected_cookie, f'Cookie is not correct: {cookie} != {expected_cookie}'
