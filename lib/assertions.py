c07692ffad847b55c0d97e79f3c6ededf3eadc37
Roman <r.zamorskiy@yandex.ru>
2023-03-17 15:45:46 +0300

    fix

--- lib/assertions.py
+++ lib/assertions.py
@@ -3,7 +3,7 @@ import json
 
 class Assertions:
     @staticmethod
-    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message): ### Проверка значения ключа JSON по его имени
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
@@ -13,7 +13,7 @@ class Assertions:
         assert response_as_dict[name] == expected_value, error_message
 
     @staticmethod
-    def assert_json_has_key(response: Response, name):
+    def assert_json_has_key(response: Response, name): ### Проверка наличия ключа в JSON по его имени
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
@@ -22,7 +22,7 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesnt have key'{name}'"
 
     @staticmethod
-    def assert_json_has_keys(response: Response, names: list):
+    def assert_json_has_keys(response: Response, names: list): ### Проверка наличия множества ключей в JSON по их имени
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
@@ -31,7 +31,7 @@ class Assertions:
             assert name in response_as_dict, f"Response JSON doesnt have key'{name}'"
 
     @staticmethod
-    def assert_json_has_not_key(response: Response, name):
+    def assert_json_has_not_key(response: Response, name): ### Проверка отсутствия ключа в JSON по его имени
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
@@ -40,6 +40,15 @@ class Assertions:
         assert name not in response_as_dict, f"Response JSON have key'{name}'. Expected its not"
 
     @staticmethod
-    def assert_status_code(response: Response, expected_status_code):
+    def assert_json_has_not_keys(response: Response, names: list): ### Проверка отсутствия множества ключей в JSON по их имени
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format, Response text is '{response.text}'"
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON have key'{name}'. Expected its not"
+
+    @staticmethod
+    def assert_status_code(response: Response, expected_status_code): ### Проверка соответствия статус кода
         assert response.status_code == expected_status_code, \
             f'Unexpected status-code: {response.status_code}. Expected: {expected_status_code}'
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -5,11 +5,10 @@ from lib.assertions import Assertions
 class TestUserGet(BaseCase):
     def test_user_get_not_auth(self):
         response = requests.get('https://playground.learnqa.ru/api/user/2')
+        unexpected_keys = ["email", "firstName", "lastName"]
 
         Assertions.assert_json_has_key(response, 'username')
-        Assertions.assert_json_has_not_key(response, 'email')
-        Assertions.assert_json_has_not_key(response, 'firstName')
-        Assertions.assert_json_has_not_key(response, 'lastName'
+        Assertions.assert_json_has_not_keys(response, unexpected_keys)
 
     def test_user_get_auth_same_user(self):
         data = {
@@ -17,13 +16,13 @@ class TestUserGet(BaseCase):
             'password': '1234'
         }
 
-        response1 = requests.get('https://playground.learnqa.ru/api/user/login', data=data)
+        response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
 
         auth_sid = self.get_cooke(response1, 'auth_sid')
         token = self.get_header(response1, 'x-csrf-token')
         user_id_from_auth = self.get_json_value(response1, 'user_id')
 
-        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth}", headers= {"x-csrf-token": token}, cookies= {"auth_sid": auth_sid})
+        response2 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id_from_auth}", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
 
-        expected_keys = ["Username", "email", "firstName", "lastName"]
+        expected_keys = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_keys)
