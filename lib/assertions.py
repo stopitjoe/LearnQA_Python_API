35a168689eca31826ef4d51296d6c065e90edfb9
Roman <r.zamorskiy@yandex.ru>
2023-03-17 14:40:05 +0300

    added new tests

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,3 +11,35 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesnt have key'{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format, Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesnt have key'{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format, Response text is '{response.text}'"
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesnt have key'{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format, Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON have key'{name}'. Expected its not"
+
+    @staticmethod
+    def assert_status_code(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f'Unexpected status-code: {response.status_code}. Expected: {expected_status_code}'
--- tests/test_headers_method.py
+++ tests/test_headers_method.py
@@ -7,3 +7,24 @@ class TestHeadersMethod:
         headers = requests.get('https://playground.learnqa.ru/api/homework_header').headers['x-secret-homework-header']
         print(f'Headers is "{headers}"')
         assert str(headers) == str(expected_header), f'Header is not correct: {expected_header} != {headers}'
+
+# class TestHeadersMethod:
+#     def test_method_headers(self):
+#         api_url = 'https://playground.learnqa.ru/api/homework_header'
+#         expected_header_value = "Some secret value"
+#
+#         try:
+#             response = requests.get(api_url)
+#
+#             print(f'Response status code: {response.status_code}')
+#             print(f'Response text: {response.text}')
+#
+#             if 'x-secret-homework-header' in response.headers:
+#                 header_value = response.headers['x-secret-homework-header']
+#
+#                 assert header_value == expected_header_value, f'Header value "{header_value}" does not match expected value "{expected_header_value}"'
+#             else:
+#                 raise AssertionError('Header x-secret-homework-header not found in response headers')
+#
+#         except requests.exceptions.RequestException as e:
+#             raise AssertionError(f'Error making request to API: {e}')
