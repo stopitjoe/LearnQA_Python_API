1b5cb72dce297969604e6e037a374e2e49e5784f
Roman <r.zamorskiy@yandex.ru>
2023-03-21 13:24:30 +0300

    added tests in register

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,10 +1,23 @@
+import allure
+import pytest
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
+@allure.epic("Register cases")
 class TestUserRegister(BaseCase):
-    def test_create_user_success(self):
+    exclude_params = [
+        ('password'),
+        ('username'),
+        ('firstName'),
+        ('lastName'),
+        ('email')
+    ]
+
+    @allure.description("Create user success")
+    def test_create_user_success(self): #------------------------------------------- Успешное создание пользователя.
         data = self.prepare_registration_data()
 
         response = MyRequests.post("/user/", data=data)
@@ -12,7 +25,8 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 200)
         Assertions.assert_json_has_key(response, 'id')
 
-    def test_create_user_with_existing_email(self):
+    @allure.description("Create user with existing email")
+    def test_create_user_with_existing_email(self): #-------------------------------- Не успешное создание пользователя с существующим email
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
 
@@ -21,3 +35,65 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", \
             f"Content is not correct - {response.content}"
+
+    @allure.description("Create user with incorrect email - missing '@'")
+    def test_create_user_incorrect_email(self):#----------------------------------- Не успешное создание пользователя с некорректным email
+        email = 'vinkotovexample.com'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode('utf-8') == f"Invalid email format", \
+            f"Content is not correct - {response.content}"
+
+    @allure.description("Create user without some parameter")
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_create_user_without_param(self, condition): #-------------------------- Не успешное создание пользователя с отсутствием одного параметра
+        if condition == 'password':
+            data = self.prepare_registration_data()
+            del data[condition]
+
+            response = MyRequests.post("/user/", data=data)
+
+            Assertions.assert_status_code(response, 400)
+            assert response.content.decode('utf-8') == f"The following required params are missed: {condition}"
+
+        elif condition == 'username':
+            data = self.prepare_registration_data()
+            del data[condition]
+
+            response = MyRequests.post("/user/", data=data)
+
+            Assertions.assert_status_code(response, 400)
+            assert response.content.decode('utf-8') == f"The following required params are missed: {condition}"
+
+        elif condition == 'firstName':
+            data = self.prepare_registration_data()
+            del data[condition]
+
+            response = MyRequests.post("/user/", data=data)
+
+            Assertions.assert_status_code(response, 400)
+            assert response.content.decode('utf-8') == f"The following required params are missed: {condition}"
+
+        elif condition == 'lastName':
+            data = self.prepare_registration_data()
+            del data[condition]
+
+            response = MyRequests.post("/user/", data=data)
+
+            Assertions.assert_status_code(response, 400)
+            assert response.content.decode('utf-8') == f"The following required params are missed: {condition}"
+
+        elif condition == 'email':
+            data = self.prepare_registration_data()
+            del data[condition]
+
+            response = MyRequests.post("/user/", data=data)
+
+            Assertions.assert_status_code(response, 400)
+            assert response.content.decode('utf-8') == f"The following required params are missed: {condition}"
+
+        else:
+            print(f"Incorrect data:{self.prepare_registration_data()}")
